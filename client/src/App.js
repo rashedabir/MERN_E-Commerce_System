a00a7167b453577c4a8b4487acdb0176f3b1a720
Rashed Khan <abir97bd@gmail.com>
2021-05-11 20:17:46 +0600

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { DataProvider } from "./GlobalState";
 import Header from "./component/Header";
 import MainPage from "./screens/MainPage";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
-    <DataProvider>
-      <Router>
-        <div className="container app">
-          <Header />
-          <MainPage />
-        </div>
-      </Router>
-    </DataProvider>
+    <div>
+      <DataProvider>
+        <Router>
+          <div className="container app">
+            <Header />
+            <MainPage />
+          </div>
+        </Router>
+      </DataProvider>
+      <MessengerCustomerChat pageId="103973174662277" appId="162221891965882" />
+    </div>
   );
 }
 
